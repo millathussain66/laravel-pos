bb40c3a19bf6220708b18c32794af7faf569fc26
MD.Millat Hussain <89291761+millat127166@users.noreply.github.com>
2022-01-03 06:52:47 +0600

    Initial commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+     
+    </div>
+    <strong>Copyright &copy; 2022-2022 <a href="http://facebook.com/millat127166">Millat Hussain</a>.</strong> All rights
+    reserved.
+  </footer>
